190051082b3dc165db4745194f504fac36e669b7
Dev Shah <djshah2903@gmail.com>
2026-08-21 20:52:14 -0400

    JS Exercises Practice

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseStr = "";
+
+    for (let i = string.length-1; i >= 0; i--){
+        reverseStr += string[i]
+    }
+
+    return reverseStr
 
 };
 
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for (let i=0; i < nums.length; i++){
+        index = arr.indexOf(nums[i])
+        while (index !== -1){
+            arr.splice(index, 1)
+            index = arr.indexOf(nums[i])
+        }
+    }
+    return arr
+
 };
 
 // Do not edit below this line
