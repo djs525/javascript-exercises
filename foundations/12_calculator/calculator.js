55902d10bc0034bff6edca3eec6fce50627353b5
Dev Shah <djshah2903@gmail.com>
2026-08-24 18:10:24 -0400

    JS Exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+  let sum = 0
+	for (let i=0; i<arr.length;i++){
+    sum += arr[i]
+  }
+  return sum
+};
+
+const multiply = function(arr) {
+  let prod = 1;
+  for (let i=0; i<arr.length; i++){
+    prod *= arr[i]
+  }
+  return prod
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b)
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	fac = 1
+  for (let i = num; i > 0; i--){
+    fac *= i
+  }
+  return fac
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    l = 0
+    r = string.length - 1
+    string = string.toLowerCase()
+
+    function isAlphanumeric(str) {
+        return /^[a-zA-Z0-9]+$/.test(str);
+    }
+
+    while (l <= r) {
+        if (!isAlphanumeric(string[l])){
+            l += 1
+            continue
+        }
+        if (!isAlphanumeric(string[r])){
+            r -= 1
+            continue
+        }
+
+        if (string[l] != string[r]){
+            return false
+        }
+        l += 1
+        r -= 1
+        
+    }
+    return true
 
 };
 
