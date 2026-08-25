071ba00d057a3e636db1c1be1778a967a330bc70
Dev Shah <djshah2903@gmail.com>
2026-08-24 22:10:55 -0400

    Finished JS Intro Exercises

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    member = parseInt(member)
+    if (member === 0){
+        return 0;
+    } else if (member < 0){
+        return "OOPS"
+    }
+    one = 1
+    two = 1
+    for (let i = 0; i < member - 2; i ++){
+        temp = one 
+        one = one + two
+        two = temp
+    }
+    return one
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const curAge = getAge(curPerson.yearOfBirth, curPerson.yearOfDeath);
+        return oldestAge < curAge ? curPerson : oldest
+    });
 
 };
 
